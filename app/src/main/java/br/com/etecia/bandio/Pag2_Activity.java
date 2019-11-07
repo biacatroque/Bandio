6d1e806de17f519b84e41cb6aeb2beb14550ef0b
Davi3000 <nearwiner1234567@gmail.com>
2019-11-06 22:10:15 -0300

    Faltou a Toolbar

--- app/src/main/java/br/com/etecia/bandio/Pag2_Activity.java
+++ app/src/main/java/br/com/etecia/bandio/Pag2_Activity.java
@@ -3,15 +3,12 @@ package br.com.etecia.bandio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
-Toolbar toolbar;
+public class Pag2_Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.pag2_layout);
     }
 }
